229d3dc721975f5fb84446ac9e9d98610092e5ed
TheMaxMur <muravjev.mak@yandex.ru>
2024-08-09 21:33:02 +0300

    home/modules: Add hyprpaper

---
+++ home/modules/hyprpaper/default.nix
@@ -0,0 +1,27 @@
+{ pkgs
+, lib
+, config
+, ...
+}:
+
+with lib;
+
+let
+  cfg = config.module.hyprpaper;
+in {
+  options = {
+    module.hyprpaper.enable = mkEnableOption "Enables hyprpaper";
+  };
+
+  config = mkIf cfg.enable {
+    services.hyprpaper = {
+      enable = true;
+
+      settings = {
+        preload = [ "../../../assets/grey_gradient.png" ];
+        wallpapers = [ "../../../assets/grey_gradient.png" ];
+      };
+    };
+  };
+}
+
