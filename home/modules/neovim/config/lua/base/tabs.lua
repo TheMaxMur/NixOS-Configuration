e70e391a7e6ad84b0c69f78a3b7912cd66e0832a
TheMaxMur <muravjev.mak@yandex.ru>
2024-08-09 21:58:48 +0300

    home/modules: Some magic with vim tabs???

--- home/modules/neovim/config/lua/base/tabs.lua
+++ home/modules/neovim/config/lua/base/tabs.lua
@@ -1,11 +1,12 @@
+vim.opt.tabstop = 2
 vim.opt.shiftwidth = 2
 vim.opt.softtabstop = 2
-vim.opt.tabstop = 2
-vim.opt.smartindent = true
 vim.opt.expandtab = true
+vim.opt.smartindent = true
+vim.g.editorconfig = false
 
 local function check_git_repo()
-	local cmd = "git rev-parse --is-inside-work-tree"
+    local cmd = "git rev-parse --is-inside-work-tree"
 	if vim.fn.system(cmd) == "true\n" then
 		vim.api.nvim_exec_autocmds("User", { pattern = "InGitRepo" })
 		return true
