4cf29cdc38b935c7d3591136d148d5b020f5ed3e
TheMaxMur <muravjev.mak@yandex.ru>
2024-08-12 17:48:03 +0300

    home/modules: Add gpg key for git

--- home/modules/git/default.nix
+++ home/modules/git/default.nix
@@ -16,8 +16,20 @@ in {
     # Git config
     programs.git = {
       enable = true;
-      userName = "maxmur";
+      userName = "TheMaxMur";
       userEmail = "muravjev.mak@yandex.ru";
+
+      signing = {
+        key = "EC9C10ED7A62D7BDB796B05EEB757CE80A4B2F30";
+        signByDefault = true;
+      };
+
+      includes = [
+        {
+          path = "~/Code/work/gitconfig";
+          condition = "gitdir:~/Code/work/**";
+        }
+      ];
     };
   };
 }
