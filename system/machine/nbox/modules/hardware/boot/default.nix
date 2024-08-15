4fce2d2f0d9e90a047361cc0ca04ae9cb8b68a55
TheMaxMur <muravjev.mak@yandex.ru>
2024-08-15 22:23:51 +0300

    system/machine/nbox: Enable lanzaboote secureboot

--- system/machine/nbox/modules/hardware/boot/default.nix
+++ system/machine/nbox/modules/hardware/boot/default.nix
@@ -1,21 +1,22 @@
 { inputs
+, lib
 , ...
 }:
 
 {
   # Bootloader settings
-  # imports = [
-  #  inputs.lanzaboote.nixosModules.lanzaboote
-  # ];
+  imports = [
+   inputs.lanzaboote.nixosModules.lanzaboote
+  ];
 
   boot = {
-    loader.systemd-boot.enable = true;
+    loader.systemd-boot.enable = lib.mkForce false;
     loader.efi.canTouchEfiVariables = true;
 
-    # lanzaboote = {
-    #  enable = true;
-    #  pkiBundle = "/etc/secureboot";
-    #};
+    lanzaboote = {
+     enable = true;
+     pkiBundle = "/etc/secureboot";
+    };
   };
 }
 
