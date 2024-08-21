bca7fe92b18c0dbef808f269c0ecd054b5520c6a
TheMaxMur <muravjev.mak@yandex.ru>
2024-08-21 12:51:02 +0300

    system/modules: Add options for homepage-dashboard module

--- system/modules/services/homepage-dashboard/default.nix
+++ system/modules/services/homepage-dashboard/default.nix
@@ -1,48 +1,63 @@
 { pkgs
+, lib
+, config
 , ...
 }:
 
+with lib;
+
+let
+  cfg = config.module.services.homepage-dashboard;
+in 
 {
-  services.homepage-dashboard = {
-    enable = true;
-    package = pkgs.homepage-dashboard;
-  };
-
-  /*
-  systemd.services.homepage-dashboard.environment.HOMEPAGE_CONFIG_DIR = let
-    configDir = pkgs.linkFarm "homepage-dashboard-config" {
-      "settings.yaml" = yaml.generate "settings.yaml" {
-        title = "Germond Homelab";
-        background = "https://images.unsplash.com/photo-1502790671504-542ad42d5189?auto=format&fit=crop&w=2560&q=80";
-        cardBlur = "sm";
-        theme = "dark";
-        color = "zinc";
-        iconStyle = "theme";
-        statusStyle = "dot";
-
-        language = "fr";
-
-        target = "_blank"; # open links in new tabs
-
-        hideVersion = true;
-        disableCollapse = true;
-
-        logpath = pkgs.linkFarm "homepage-dashboard-null-logs" {
-          "logs/homepage.log" = "/dev/null";
-        };
-      };
-      "services.yaml" = yaml.generate "services.yaml" [];
-      "widgets.yaml" = yaml.generate "widgets.yaml" [];
-      "bookmarks.yaml" = yaml.generate "bookmarks.yaml" [];
-      "docker.yaml" = yaml.generate "docker.yaml" {};
-      "kubernetes.yaml" = yaml.generate "kubernetes.yaml" {
-        mode = "disabled";
-      };
-      "custom.css" = pkgs.writeText "custom.css" '''';
-      "custom.js" = pkgs.writeText "custom.js" '''';
+  options = {
+    module.services.homepage-dashboard = {
+      enable = mkEnableOption "Enables homepage-dashboard";
     };
-  in
-    lib.mkForce "${configDir}";
-  */
+  };
+  
+  config = mkIf cfg.enable {
+    services.homepage-dashboard = {
+      enable = true;
+      package = pkgs.homepage-dashboard;
+    };
+
+    /*
+    systemd.services.homepage-dashboard.environment.HOMEPAGE_CONFIG_DIR = let
+      configDir = pkgs.linkFarm "homepage-dashboard-config" {
+        "settings.yaml" = yaml.generate "settings.yaml" {
+          title = "Germond Homelab";
+          background = "https://images.unsplash.com/photo-1502790671504-542ad42d5189?auto=format&fit=crop&w=2560&q=80";
+          cardBlur = "sm";
+          theme = "dark";
+          color = "zinc";
+          iconStyle = "theme";
+          statusStyle = "dot";
+
+          language = "fr";
+
+          target = "_blank"; # open links in new tabs
+
+          hideVersion = true;
+          disableCollapse = true;
+
+          logpath = pkgs.linkFarm "homepage-dashboard-null-logs" {
+            "logs/homepage.log" = "/dev/null";
+          };
+        };
+        "services.yaml" = yaml.generate "services.yaml" [];
+        "widgets.yaml" = yaml.generate "widgets.yaml" [];
+        "bookmarks.yaml" = yaml.generate "bookmarks.yaml" [];
+        "docker.yaml" = yaml.generate "docker.yaml" {};
+        "kubernetes.yaml" = yaml.generate "kubernetes.yaml" {
+          mode = "disabled";
+        };
+        "custom.css" = pkgs.writeText "custom.css" '''';
+        "custom.js" = pkgs.writeText "custom.js" '''';
+      };
+    in
+      lib.mkForce "${configDir}";
+    */
+  };
 }
 
