dccefecb74bd66a3036914c3bd141510e76b48c7
TheMaxMur <muravjev.mak@yandex.ru>
2024-08-30 17:05:48 +0300

    modules: Add chaotic cache

--- modules/nix/default.nix
+++ modules/nix/default.nix
@@ -42,8 +42,16 @@ in {
       settings = {
         experimental-features = [ "nix-command" "flakes" ];
         auto-optimise-store = true;
-        substituters = [ "https://hyprland.cachix.org" ];
-        trusted-public-keys = [ "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc=" ];
+
+        substituters = [
+          "https://hyprland.cachix.org"
+          "https://nyx.chaotic.cx"
+        ];
+
+        trusted-public-keys = [
+          "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+          "chaotic-nyx.cachix.org-1:HfnXSw4pj95iI/n17rIDy40agHj12WfF+Gqk6SonIT8="
+        ];
       };
 
       gc = {
