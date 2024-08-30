801f93342103671e72a8a82ad0da031e5bcd720d
TheMaxMur <muravjev.mak@yandex.ru>
2024-08-30 17:04:11 +0300

    system/modules: Move unbound to services

--- system/modules/pihole/default.nix
+++ system/modules/pihole/default.nix
@@ -13,45 +13,6 @@ in {
   };
 
   config = mkIf cfg.enable {
-    services.unbound = {
-      enable = true;
-
-      settings.server = {
-        port = "5335";
-        auto-trust-anchor-file = "/var/lib/unbound/root.key";
-        root-hints = "/var/lib/unbound/root.hints";
-        verbosity = "0";
-        interface = "172.17.0.1";
-        do-ip4 = "yes";
-        do-udp = "yes";
-        do-tcp = "yes";
-        do-ip6 = "no";
-        prefer-ip6 = "no";
-        harden-glue = "yes";
-        harden-dnssec-stripped = "yes";
-        use-caps-for-id  = "no";
-        edns-buffer-size = "1232";
-        prefetch = "yes";
-        num-threads = "1";
-        so-rcvbuf = "1m";
-
-        access-control = [
-          "192.168.1.0/24 allow"
-          "172.16.0.0/12 allow"
-          "127.0.0.0/8 allow"
-          "10.0.0.0/8 allow"
-        ];
-
-        private-address = [
-          "192.168.0.0/16"
-          "169.254.0.0/16"
-          "172.16.0.0/12"
-          "127.0.0.0/8"
-          "10.0.0.0/8"
-        ];
-      };
-    };
-
     virtualisation.oci-containers = {
       backend = "docker";
 
