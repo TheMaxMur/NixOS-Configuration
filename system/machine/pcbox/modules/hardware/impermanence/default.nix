743752ca533f5745ccde58e68284246cf74673b6
TheMaxMur <muravjev.mak@yandex.ru>
2024-08-30 17:10:16 +0300

    system/machine/pcbox: Remove some path from impermanence

--- system/machine/pcbox/modules/hardware/impermanence/default.nix
+++ system/machine/pcbox/modules/hardware/impermanence/default.nix
@@ -34,10 +34,6 @@ _:
         "/etc/ssh/ssh_host_ed25519_key.pub"
         "/etc/ssh/ssh_host_rsa_key"
         "/etc/ssh/ssh_host_rsa_key.pub"
-        "/var/lib/NetworkManager/NetworkManager.state"
-        "/var/lib/NetworkManager/secret_key"
-        "/var/lib/NetworkManager/seen-bssids"
-        "/var/lib/NetworkManager/timestamps"
       ];
     };
   };
