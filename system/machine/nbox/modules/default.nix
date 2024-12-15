5a84632f2d34ef44120d85bee1caf56b3251ca8d
TheMaxMur <muravjev.mak@yandex.ru>
2024-12-15 19:20:09 +0300

    system/machine/nbox: Fix paths

--- system/machine/nbox/modules/default.nix
+++ system/machine/nbox/modules/default.nix
@@ -1,11 +1,14 @@
-{ lib
-, machineModulesPath
+{ self
+, lib
+, machineDir
 , ...
 }:
 
-{
+let
+  modules = "${self}/system/machine/${machineDir}/modules";
+in {
   imports = builtins.filter (module: lib.pathIsDirectory module) (
-    map (module: "${machineModulesPath}/${module}") (builtins.attrNames (builtins.readDir machineModulesPath))
+    map (module: "${modules}/${module}") (builtins.attrNames (builtins.readDir modules))
   );
 }
 
