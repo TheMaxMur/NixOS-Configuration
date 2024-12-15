e9d87cd6a68a2a8a5ebf227d5ddb8014fa4785bf
TheMaxMur <muravjev.mak@yandex.ru>
2024-12-15 19:21:36 +0300

    system/machine/pcbox: Fix paths

--- system/machine/pcbox/modules/hardware/default.nix
+++ system/machine/pcbox/modules/hardware/default.nix
@@ -1,10 +1,11 @@
-{ lib
-, machineModulesPath
+{ self
+, machineDir
+, lib
 , ...
 }:
 
 let
-  hardwareModulesPath = "${machineModulesPath}/hardware";
+  hardwareModulesPath = "${self}/system/machine/${machineDir}/modules/hardware";
 in {
   imports = builtins.filter (module: lib.pathIsDirectory module) (
     map (module: "${hardwareModulesPath}/${module}") (builtins.attrNames (builtins.readDir hardwareModulesPath))
