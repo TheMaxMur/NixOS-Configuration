db57a3dceb3fd25fb04fbb2b13ee0a797b8f40a4
TheMaxMur <muravjev.mak@yandex.ru>
2024-12-15 19:12:23 +0300

    home/modules: Add bindkeysToCode for sway

--- home/modules/sway/keybinds/default.nix
+++ home/modules/sway/keybinds/default.nix
@@ -63,6 +63,7 @@ in {
     wayland.windowManager.sway.config = {
       inherit terminal;
       modifier = super;
+      bindkeysToCode = true;
 
       assigns = {
         "2" = [ { app_id = "org.telegram.desktop"; } ];
@@ -75,36 +76,36 @@ in {
         "${super}+Return" = "exec ${terminal}";
 
         # Kill active window
-        "--to-code ${super}+q" = "kill";
+        "${super}+q" = "kill";
 
         # PowerMenu
         "--to-code ${super}+p" = "exec ${powerMenu}/bin/powerMenu.sh";
 
         # Change focus
         # Vim like
-        "--to-code ${super}+h" = "focus left";
-        "--to-code ${super}+j" = "focus down";
-        "--to-code ${super}+k" = "focus up";
-        "--to-code ${super}+l" = "focus right";
+        "${super}+h" = "focus left";
+        "${super}+j" = "focus down";
+        "${super}+k" = "focus up";
+        "${super}+l" = "focus right";
 
         # With arrows
-        "--to-code ${super}+Left"  = "focus left";
-        "--to-code ${super}+Down"  = "focus down";
-        "--to-code ${super}+Up"    = "focus up";
-        "--to-code ${super}+Right" = "focus right";
+        "${super}+Left"  = "focus left";
+        "${super}+Down"  = "focus down";
+        "${super}+Up"    = "focus up";
+        "${super}+Right" = "focus right";
 
         # Move focused window
         # Vim like
-        "--to-code ${super}+Shift+h" = "move left";
-        "--to-code ${super}+Shift+j" = "move down";
-        "--to-code ${super}+Shift+k" = "move up";
-        "--to-code ${super}+Shift+l" = "move right";
+        "${super}+Shift+h" = "move left";
+        "${super}+Shift+j" = "move down";
+        "${super}+Shift+k" = "move up";
+        "${super}+Shift+l" = "move right";
 
         # With arrows
-        "--to-code ${super}+Shift+Left"  = "move left";
-        "--to-code ${super}+Shift+Down"  = "move down";
-        "--to-code ${super}+Shift+Up"    = "move up";
-        "--to-code ${super}+Shift+Right" = "move right";
+        "${super}+Shift+Left"  = "move left";
+        "${super}+Shift+Down"  = "move down";
+        "${super}+Shift+Up"    = "move up";
+        "${super}+Shift+Right" = "move right";
 
         # Application launcher
         "${super}+d" = "exec ${appLauncher}";
@@ -125,56 +126,56 @@ in {
         "Shift+Print" = "exec ${screenshotScreen}";
 
         # Reload the configuration file
-        "--to-code ${super}+Shift+r" = "reload";
+        "${super}+Shift+r" = "reload";
 
         # Fullscreen mode
-        "--to-code ${super}+f" = "fullscreen";
+        "${super}+f" = "fullscreen";
 
         # Float mode
-        "--to-code ${super}+Space" = "floating toggle";
+        "${super}+Space" = "floating toggle";
 
         # Resize mode
-        "--to-code ${super}+r" = "mode resize";
+        "${super}+r" = "mode resize";
 
         # Layouts
-        "--to-code ${super}+v" = "splitv";
-        "--to-code ${super}+b" = "splith";
-        "--to-code ${super}+s" = "layout toggle split";
-        "--to-code ${super}+t" = "layout tabbed";
+        "${super}+v" = "splitv";
+        "${super}+b" = "splith";
+        "${super}+t" = "layout toggle split tabbed";
+        "${super}+a" = "focus parent";
 
         # Switch to workspace using number row
-        "--to-code ${super}+1"     = "${workspace1}";
-        "--to-code ${super}+2"     = "${workspace2}";
-        "--to-code ${super}+3"     = "${workspace3}";
-        "--to-code ${super}+4"     = "${workspace4}";
-        "--to-code ${super}+5"     = "${workspace5}";
-        "--to-code ${super}+6"     = "${workspace6}";
-        "--to-code ${super}+7"     = "${workspace7}";
-        "--to-code ${super}+8"     = "${workspace8}";
-        "--to-code ${super}+9"     = "${workspace9}";
-        "--to-code ${super}+0"     = "${workspace10}";
-        "--to-code ${super}+minus" = "${workspace11}";
-        "--to-code ${super}+equal" = "${workspace12}";
+        "${super}+1"     = "${workspace1}";
+        "${super}+2"     = "${workspace2}";
+        "${super}+3"     = "${workspace3}";
+        "${super}+4"     = "${workspace4}";
+        "${super}+5"     = "${workspace5}";
+        "${super}+6"     = "${workspace6}";
+        "${super}+7"     = "${workspace7}";
+        "${super}+8"     = "${workspace8}";
+        "${super}+9"     = "${workspace9}";
+        "${super}+0"     = "${workspace10}";
+        "${super}+minus" = "${workspace11}";
+        "${super}+equal" = "${workspace12}";
 
         # Switch to scratchpad
-        "--to-code ${super}+z" = "scratchpad show";
+        "${super}+z" = "scratchpad show";
 
         # Move window to workspace using number row
-        "--to-code ${super}+Shift+1"     = "move container to ${workspace1}";
-        "--to-code ${super}+Shift+2"     = "move container to ${workspace2}";
-        "--to-code ${super}+Shift+3"     = "move container to ${workspace3}";
-        "--to-code ${super}+Shift+4"     = "move container to ${workspace4}";
-        "--to-code ${super}+Shift+5"     = "move container to ${workspace5}";
-        "--to-code ${super}+Shift+6"     = "move container to ${workspace6}";
-        "--to-code ${super}+Shift+7"     = "move container to ${workspace7}";
-        "--to-code ${super}+Shift+8"     = "move container to ${workspace8}";
-        "--to-code ${super}+Shift+9"     = "move container to ${workspace9}";
-        "--to-code ${super}+Shift+0"     = "move container to ${workspace10}";
-        "--to-code ${super}+Shift+minus" = "move container to ${workspace11}";
-        "--to-code ${super}+Shift+equal" = "move container to ${workspace12}";
+        "${super}+Shift+1"     = "move container to ${workspace1}";
+        "${super}+Shift+2"     = "move container to ${workspace2}";
+        "${super}+Shift+3"     = "move container to ${workspace3}";
+        "${super}+Shift+4"     = "move container to ${workspace4}";
+        "${super}+Shift+5"     = "move container to ${workspace5}";
+        "${super}+Shift+6"     = "move container to ${workspace6}";
+        "${super}+Shift+7"     = "move container to ${workspace7}";
+        "${super}+Shift+8"     = "move container to ${workspace8}";
+        "${super}+Shift+9"     = "move container to ${workspace9}";
+        "${super}+Shift+0"     = "move container to ${workspace10}";
+        "${super}+Shift+minus" = "move container to ${workspace11}";
+        "${super}+Shift+equal" = "move container to ${workspace12}";
 
         # Move window to scratchpad
-        "--to-code ${super}+Shift+z" = "move scratchpad";
+        "${super}+Shift+z" = "move scratchpad";
       };
 
       modes = {
