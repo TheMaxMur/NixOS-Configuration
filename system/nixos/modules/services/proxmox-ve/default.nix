b2ef58c94c8628d09c64cd67c486a2d5b305039d
TheMaxMur <muravjev.mak@yandex.ru>
2025-01-21 11:37:39 +0300

    system/nixos/modules: Add ipAddress option for proxmox-ve

--- system/nixos/modules/services/proxmox-ve/default.nix
+++ system/nixos/modules/services/proxmox-ve/default.nix
@@ -1,21 +1,29 @@
-{ config
-, lib
-, ...
+{
+  config,
+  lib,
+  ...
 }:
 
 with lib;
 
 let
   cfg = config.module.services.proxmox-ve;
-in {
+in
+{
   options.module.services.proxmox-ve = {
     enable = mkEnableOption "Enable module proxmox-ve";
+
+    ipAddress = mkOption {
+      type = types.str;
+      default = "";
+      description = "IP address for proxmox";
+    };
   };
 
   config = mkIf cfg.enable {
     services.proxmox-ve = {
       enable = true;
+      inherit (cfg) ipAddress;
     };
   };
 }
-
