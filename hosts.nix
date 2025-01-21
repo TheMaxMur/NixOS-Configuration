a6d133d045494112763b0c54c415f94d3c5cfe94
TheMaxMur <muravjev.mak@yandex.ru>
2025-01-21 11:06:36 +0300

    hosts: Fmt

--- hosts.nix
+++ hosts.nix
@@ -1,43 +1,42 @@
 {
   nixos = {
     pcbox = {
-      username      = "maxmur";
-      platform      = "x86_64-linux";
-      stateVersion  = "25.05";
+      username = "maxmur";
+      platform = "x86_64-linux";
+      stateVersion = "25.05";
       isWorkstation = true;
-      wm            = "sway";
+      wm = "sway";
     };
 
     nbox = {
-      username      = "maxmur";
-      platform      = "x86_64-linux";
-      stateVersion  = "25.05";
+      username = "maxmur";
+      platform = "x86_64-linux";
+      stateVersion = "25.05";
       isWorkstation = true;
-      wm            = "sway";
+      wm = "sway";
     };
 
     rasp = {
-      username      = "maxmur";
-      platform      = "aarch64-linux";
-      stateVersion  = "25.05";
+      username = "maxmur";
+      platform = "aarch64-linux";
+      stateVersion = "25.05";
       isWorkstation = false;
     };
 
     hlbox = {
-      username      = "maxmur";
-      platform      = "x86_64-linux";
-      stateVersion  = "25.05";
+      username = "maxmur";
+      platform = "x86_64-linux";
+      stateVersion = "25.05";
       isWorkstation = false;
     };
   };
 
   darwin = {
     macbox = {
-      username      = "maxmur";
-      platform      = "aarch64-darwin";
-      stateVersion  = 5;
+      username = "maxmur";
+      platform = "aarch64-darwin";
+      stateVersion = 5;
       isWorkstation = true;
     };
   };
 }
-
