2376affe8e2ea9b22de6231f88b4a4e881169bb4
TheMaxMur <muravjev.mak@yandex.ru>
2025-01-21 11:20:48 +0300

    flake: Change lanzaboote input

--- flake.nix
+++ flake.nix
@@ -109,7 +109,7 @@
     };
 
     lanzaboote = {
-      url = "github:nix-community/lanzaboote/v0.3.0";
+      url = "github:nix-community/lanzaboote/v0.4.1";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
