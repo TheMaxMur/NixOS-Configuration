0a91d269dd38f11abc7e9395316d6620c1297bc8
TheMaxMur <muravjev.mak@yandex.ru>
2025-01-21 11:25:01 +0300

    home/modules: Sway fmt

--- home/modules/sway/keybinds/default.nix
+++ home/modules/sway/keybinds/default.nix
@@ -1,7 +1,8 @@
-{ config
-, lib
-, pkgs
-, ...
+{
+  config,
+  lib,
+  pkgs,
+  ...
 }:
 
 with lib;
@@ -10,33 +11,33 @@ let
   cfg = config.module.sway.keybindings;
   super = "Mod4";
 
-  workspace1  = "workspace number 1";
-  workspace2  = "workspace number 2";
-  workspace3  = "workspace number 3";
-  workspace4  = "workspace number 4";
-  workspace5  = "workspace number 5";
-  workspace6  = "workspace number 6";
-  workspace7  = "workspace number 7";
-  workspace8  = "workspace number 8";
-  workspace9  = "workspace number 9";
+  workspace1 = "workspace number 1";
+  workspace2 = "workspace number 2";
+  workspace3 = "workspace number 3";
+  workspace4 = "workspace number 4";
+  workspace5 = "workspace number 5";
+  workspace6 = "workspace number 6";
+  workspace7 = "workspace number 7";
+  workspace8 = "workspace number 8";
+  workspace9 = "workspace number 9";
   workspace10 = "workspace number 10";
   workspace11 = "workspace number 11";
   workspace12 = "workspace number 12";
 
-  terminal          = config.module.defaults.terminalCmd;
-  appLauncher       = config.module.defaults.appLauncherCmd;
-  audioControl      = config.module.defaults.audioControlCmd;
+  terminal = config.module.defaults.terminalCmd;
+  appLauncher = config.module.defaults.appLauncherCmd;
+  audioControl = config.module.defaults.audioControlCmd;
   brightnessControl = config.module.defaults.brightnessControlCmd;
-  clipHist          = config.module.defaults.clipHistCmd;
-  notificationsApp  = config.module.defaults.notificationsAppCmd;
+  clipHist = config.module.defaults.clipHistCmd;
+  notificationsApp = config.module.defaults.notificationsAppCmd;
 
-  screenshotArea    = "${pkgs.slurp}/bin/slurp | ${pkgs.grim}/bin/grim -g - - | ${pkgs.wl-clipboard}/bin/wl-copy";
-  screenshotScreen  = "${pkgs.grim}/bin/grim -o $(swaymsg -t get_outputs | ${pkgs.jq}/bin/jq -r '.[] | select(.focused) | .name') - | ${pkgs.wl-clipboard}/bin/wl-copy";
+  screenshotArea = "${pkgs.slurp}/bin/slurp | ${pkgs.grim}/bin/grim -g - - | ${pkgs.wl-clipboard}/bin/wl-copy";
+  screenshotScreen = "${pkgs.grim}/bin/grim -o $(swaymsg -t get_outputs | ${pkgs.jq}/bin/jq -r '.[] | select(.focused) | .name') - | ${pkgs.wl-clipboard}/bin/wl-copy";
 
   powerMenu = pkgs.writeShellScriptBin "powerMenu.sh" ''
     #!/usr/bin/env bash
 
-    op=$(echo -e " Poweroff\n Reboot\n Suspend\n Lock\n Logout" | ${pkgs.wofi}/bin/wofi -i --dmenu | ${pkgs.gawk}/bin/awk '{print tolower($2)}')
+    op=$(echo -e " Poweroff\n Reboot\n Suspend\n Lock\n Logout" | ${pkgs.rofi-wayland}/bin/rofi -i -dmenu | ${pkgs.gawk}/bin/awk '{print tolower($2)}')
 
     case $op in 
       poweroff)
@@ -54,7 +55,8 @@ let
         ;;
     esac
   '';
-in {
+in
+{
   options.module.sway.keybindings = {
     enable = mkEnableOption "Enable sway keybindings";
   };
@@ -89,9 +91,9 @@ in {
         "${super}+l" = "focus right";
 
         # With arrows
-        "${super}+Left"  = "focus left";
-        "${super}+Down"  = "focus down";
-        "${super}+Up"    = "focus up";
+        "${super}+Left" = "focus left";
+        "${super}+Down" = "focus down";
+        "${super}+Up" = "focus up";
         "${super}+Right" = "focus right";
 
         # Move focused window
@@ -102,9 +104,9 @@ in {
         "${super}+Shift+l" = "move right";
 
         # With arrows
-        "${super}+Shift+Left"  = "move left";
-        "${super}+Shift+Down"  = "move down";
-        "${super}+Shift+Up"    = "move up";
+        "${super}+Shift+Left" = "move left";
+        "${super}+Shift+Down" = "move down";
+        "${super}+Shift+Up" = "move up";
         "${super}+Shift+Right" = "move right";
 
         # Application launcher
@@ -112,17 +114,17 @@ in {
         "${super}+c" = "exec ${clipHist}";
 
         # Multimedia keys
-        "xf86audioraisevolume"  = "exec ${audioControl} set-sink-volume @DEFAULT_SINK@ +5%";
-        "xf86audiolowervolume"  = "exec ${audioControl} set-sink-volume @DEFAULT_SINK@ -5%";
-        "xf86audiomute"         = "exec ${audioControl} set-sink-mute @DEFAULT_SINK@ toggle";
+        "xf86audioraisevolume" = "exec ${audioControl} set-sink-volume @DEFAULT_SINK@ +5%";
+        "xf86audiolowervolume" = "exec ${audioControl} set-sink-volume @DEFAULT_SINK@ -5%";
+        "xf86audiomute" = "exec ${audioControl} set-sink-mute @DEFAULT_SINK@ toggle";
         "XF86MonBrightnessDown" = "exec ${brightnessControl} set 5%-";
-        "XF86MonBrightnessUp"   = "exec ${brightnessControl} set +5%";
+        "XF86MonBrightnessUp" = "exec ${brightnessControl} set +5%";
 
         # Notifications
         "${super}+n" = "exec ${notificationsApp}";
 
         # Screenshot
-        "Print"       = "exec ${screenshotArea}";
+        "Print" = "exec ${screenshotArea}";
         "Shift+Print" = "exec ${screenshotScreen}";
 
         # Reload the configuration file
@@ -144,16 +146,16 @@ in {
         "${super}+a" = "focus parent";
 
         # Switch to workspace using number row
-        "${super}+1"     = "${workspace1}";
-        "${super}+2"     = "${workspace2}";
-        "${super}+3"     = "${workspace3}";
-        "${super}+4"     = "${workspace4}";
-        "${super}+5"     = "${workspace5}";
-        "${super}+6"     = "${workspace6}";
-        "${super}+7"     = "${workspace7}";
-        "${super}+8"     = "${workspace8}";
-        "${super}+9"     = "${workspace9}";
-        "${super}+0"     = "${workspace10}";
+        "${super}+1" = "${workspace1}";
+        "${super}+2" = "${workspace2}";
+        "${super}+3" = "${workspace3}";
+        "${super}+4" = "${workspace4}";
+        "${super}+5" = "${workspace5}";
+        "${super}+6" = "${workspace6}";
+        "${super}+7" = "${workspace7}";
+        "${super}+8" = "${workspace8}";
+        "${super}+9" = "${workspace9}";
+        "${super}+0" = "${workspace10}";
         "${super}+minus" = "${workspace11}";
         "${super}+equal" = "${workspace12}";
 
@@ -161,16 +163,16 @@ in {
         "${super}+z" = "scratchpad show";
 
         # Move window to workspace using number row
-        "${super}+Shift+1"     = "move container to ${workspace1}";
-        "${super}+Shift+2"     = "move container to ${workspace2}";
-        "${super}+Shift+3"     = "move container to ${workspace3}";
-        "${super}+Shift+4"     = "move container to ${workspace4}";
-        "${super}+Shift+5"     = "move container to ${workspace5}";
-        "${super}+Shift+6"     = "move container to ${workspace6}";
-        "${super}+Shift+7"     = "move container to ${workspace7}";
-        "${super}+Shift+8"     = "move container to ${workspace8}";
-        "${super}+Shift+9"     = "move container to ${workspace9}";
-        "${super}+Shift+0"     = "move container to ${workspace10}";
+        "${super}+Shift+1" = "move container to ${workspace1}";
+        "${super}+Shift+2" = "move container to ${workspace2}";
+        "${super}+Shift+3" = "move container to ${workspace3}";
+        "${super}+Shift+4" = "move container to ${workspace4}";
+        "${super}+Shift+5" = "move container to ${workspace5}";
+        "${super}+Shift+6" = "move container to ${workspace6}";
+        "${super}+Shift+7" = "move container to ${workspace7}";
+        "${super}+Shift+8" = "move container to ${workspace8}";
+        "${super}+Shift+9" = "move container to ${workspace9}";
+        "${super}+Shift+0" = "move container to ${workspace10}";
         "${super}+Shift+minus" = "move container to ${workspace11}";
         "${super}+Shift+equal" = "move container to ${workspace12}";
 
@@ -187,9 +189,9 @@ in {
           "l" = "resize shrink width 10px";
 
           # Arrows
-          "Left"  = "resize shrink width 10px";
-          "Down"  = "resize shrink height 10px";
-          "Up"    = "resize shrink height 10px";
+          "Left" = "resize shrink width 10px";
+          "Down" = "resize shrink height 10px";
+          "Up" = "resize shrink height 10px";
           "Right" = "resize shrink width 10px";
 
           # Vim like binds
@@ -199,9 +201,9 @@ in {
           "Shift+l" = "resize grow width 10px";
 
           # Arrows
-          "Shift+Left"  = "resize grow width 10px";
-          "Shift+Down"  = "resize grow height 10px";
-          "Shift+Up"    = "resize grow height 10px";
+          "Shift+Left" = "resize grow width 10px";
+          "Shift+Down" = "resize grow height 10px";
+          "Shift+Up" = "resize grow height 10px";
           "Shift+Right" = "resize grow width 10px";
 
           # Return to normal mode
@@ -212,4 +214,3 @@ in {
     };
   };
 }
-
