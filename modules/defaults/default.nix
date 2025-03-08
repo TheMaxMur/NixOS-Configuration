482f9e39f335fc30d4cdf78ba2d2bab4e78c73f8
TheMaxMur <muravjev.mak@yandex.ru>
2025-03-08 07:24:21 +0300

    defaults: Fix cliphist execs

--- modules/defaults/default.nix
+++ modules/defaults/default.nix
@@ -96,10 +96,24 @@ in
       default = "${pkgs.brightnessctl}/bin/brightnessctl";
     };
 
-    clipHistCmd = mkOption {
-      type = types.str;
-      default = "${pkgs.cliphist}/bin/cliphist list | ${cfg.appLauncherCmd} -d | ${pkgs.cliphist}/bin/cliphist decode | ${pkgs.wl-clipboard}/bin/wl-copy";
-    };
+    clipHistCmd =
+      let
+        cliphistBase = "${pkgs.cliphist}/bin/cliphist";
+        createExec =
+          launcher:
+          "${cliphistBase} list | ${launcher} | ${cliphistBase} decode | ${pkgs.wl-clipboard}/bin/wl-copy";
+
+        cliphistExecs = {
+          rofi = createExec "${pkgs.rofi}/bin/rofi -dmenu";
+          rofi-wayland = createExec "${pkgs.rofi-wayland}/bin/rofi -dmenu";
+          wofi = createExec "${pkgs.wofi}/bin/wofi --show dmenu";
+          fuzzel = createExec "${pkgs.fuzzel}/bin/fuzzel -d";
+        };
+      in
+      mkOption {
+        type = str;
+        default = cliphistExecs.${cfg.appLauncher};
+      };
 
     notificationsAppCmd = mkOption {
       type = str;
