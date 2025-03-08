09172854e5b493b49c10a1ff82f86335b22675f9
TheMaxMur <muravjev.mak@yandex.ru>
2025-03-08 07:22:27 +0300

    modules: Add allowed and trusted users for nix, and add devenv sub

--- modules/nix/default.nix
+++ modules/nix/default.nix
@@ -3,6 +3,7 @@
   lib,
   config,
   pkgs,
+  username,
   ...
 }:
 
@@ -50,11 +51,20 @@ in
           "flakes"
         ];
 
+        allowed-users = [ "@wheel" ];
+
+        trusted-users = [
+          "root"
+          username
+        ];
+
         substituters = [
           "https://hyprland.cachix.org"
           "https://nyx.chaotic.cx"
           "https://ghostty.cachix.org"
           "https://cache.saumon.network/proxmox-nixos"
+          "https://nixpkgs-python.cachix.org"
+          "https://devenv.cachix.org"
         ];
 
         trusted-public-keys = [
@@ -62,6 +72,8 @@ in
           "chaotic-nyx.cachix.org-1:HfnXSw4pj95iI/n17rIDy40agHj12WfF+Gqk6SonIT8="
           "ghostty.cachix.org-1:QB389yTa6gTyneehvqG58y0WnHjQOqgnA+wBnpWWxns="
           "proxmox-nixos:nveXDuVVhFDRFx8Dn19f1WDEaNRJjPrF2CPD2D+m1ys="
+          "nixpkgs-python.cachix.org-1:hxjI7pFxTyuTHn2NkvWCrAUcNZLNS3ZAvfYNuYifcEU="
+          "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw="
         ];
       };
     };
