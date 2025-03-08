9d841a795133547a84c1b83a279bad625991a615
TheMaxMur <muravjev.mak@yandex.ru>
2025-03-08 07:20:53 +0300

    system/nixos/modules/serivces: Irqbalance init

---
+++ system/nixos/modules/services/irqbalance/default.nix
@@ -0,0 +1,22 @@
+{
+  lib,
+  config,
+  ...
+}:
+
+let
+  inherit (lib) mkEnableOption mkIf;
+
+  cfg = config.module.services.irqbalance;
+in
+{
+  options = {
+    module.services.irqbalance.enable = mkEnableOption "Enables irqbalance";
+  };
+
+  config = mkIf cfg.enable {
+    services.irqbalance = {
+      enable = true;
+    };
+  };
+}
