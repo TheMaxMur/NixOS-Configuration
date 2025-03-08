2ad3d4536620579e6c6f0c3f273f8764165572bc
TheMaxMur <muravjev.mak@yandex.ru>
2025-03-08 07:21:59 +0300

    system/nixos/modules: Tune pipewire

--- system/nixos/modules/sound/default.nix
+++ system/nixos/modules/sound/default.nix
@@ -23,6 +23,44 @@ in
       alsa.enable = true;
       alsa.support32Bit = true;
       pulse.enable = true;
+
+      extraConfig = {
+        pipewire = {
+          "92-low-latency" = {
+            "context.properties" = {
+              "default.clock.rate" = 48000;
+              "default.clock.allowed-rates" = [
+                44100
+                48000
+                88200
+                96000
+              ];
+
+              "default.clock.min-quantum" = 512;
+              "default.clock.quantum" = 4096;
+              "default.clock.max-quantum" = 8192;
+            };
+          };
+
+          "93-no-resampling" = {
+            "context.properties" = {
+              "default.clock.rate" = 48000;
+              "default.clock.allowed-rates" = [
+                44100
+                48000
+                96000
+                192000
+              ];
+            };
+          };
+
+          "94-no-upmixing" = {
+            "stream.properties" = {
+              "channelmix.upmix" = false;
+            };
+          };
+        };
+      };
     };
   };
 }
