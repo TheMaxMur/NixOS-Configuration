836679f57e931d2682ecdfd6d2490c7e9ffb5cac
TheMaxMur <muravjev.mak@yandex.ru>
2025-03-19 08:20:48 +0300

    system/nixos/modules/services: Homepage dashboard init

---
+++ system/nixos/modules/services/homepage-dashboard/settings/default.nix
@@ -0,0 +1,28 @@
+{
+  services.homepage-dashboard.settings = {
+    title = "Welcome to Home!";
+    favicon = "https://img.icons8.com/?size=100&id=2006&format=png&color=000000";
+    background = "https://images.unsplash.com/photo-1481277542470-605612bd2d61?q=80&w=1612&auto=format&fit=crop";
+    color = "white";
+    theme = "dark";
+    iconStyle = "theme";
+    language = "en";
+
+    layout = {
+      User = {
+        style = "row";
+        columns = 4;
+      };
+
+      Media = {
+        style = "row";
+        columns = 4;
+      };
+
+      Services = {
+        style = "row";
+        columns = 4;
+      };
+    };
+  };
+}
